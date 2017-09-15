e1936c91991d2c326b69812f92ffdbc831dfa2f5
Derek <dmcfate@csumb.edu>
2017-09-15 20:04:27 +0000

    added baseline php struture

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
-<?php
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> SilverJack </title>
+        <meta charset="utf-8"/>
+        <style>
+            @import url("css/styles.css");
+        </style>
+    </head>
+    <body>
+        <div id="main">
+        <?php
+            play();
+        ?>
+        
+        <form>
+            <input type="submit" value="Spin!"/>
+        </form>
+        </div>
+    </body>
+</html>
