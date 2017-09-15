8a7122190f9992a58d9191c3269f0af19aaf3382
Jaime Velazquez <jaimegvelazquez@gmail.com>
2017-09-15 20:47:35 +0000

    added stuff that works

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php
+
+$deck = array();
+$suits = array("clubs","spades","hearts","diamonds");
+$scores = array(0, 0, 0, 0);
+
+for ($i = 0; $i < 4; $i++) {            //sets the suit 
+    for ($j = 1; $j <= 13; $j++) {      //sets the value
+    
+        $deck[] = $suits[$i] . "_" . $j;
+    }
+    
+}
+
+shuffle($deck);
+
+foreach ($deck as $card) {              //displays all values of the array for debugging 
+    echo $card . "<br />";
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -10,24 +32,17 @@
     <body>
         <div id="main">
         <?php
-            $playerState = true;
-            $playerNum = 0;
-            while($playerState){
-                if($scores[$playerNum] > 35){
-                    $playerNum++;
-                    if(playerNum > 3){
-                        break;
-                    }
-                    
+            for($i = 0; $i < 4; $i++){
+                while($scores[$i] < 35){
+                    $poppedValue = explode("_", array_pop($deck));
+                    $scores[$i] += $poppedValue[1];
                 }
-                $poppedValue = explode("_", array_pop($deck));
-                $scores[$playerNum] += $poppedValue[1];
             }
             foreach ($scores as $score){
                 echo $score . "</br>";
             }
         ?>
-        
+
         <form>
             <input type="submit" value="Spin!"/>
         </form>
