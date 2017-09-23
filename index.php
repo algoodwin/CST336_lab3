5b13cbc7351887df931df6c61ba7e7fad3e37e1d
Derek <dmcfate@csumb.edu>
2017-09-23 06:20:13 +0000

    added match counter

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+session_start();
+if(isset($_SESSION['matchCount'])) { // Checks whether the session exists
+    
+    $_SESSION['matchCount'] = 1;
+}
 
 $deck = array();
 $suits = array("clubs","spades","hearts","diamonds");
@@ -42,21 +47,47 @@ shuffle($deck);
         for($i =0; $i<4; $i++)
         {
             echo "<img src = 'people/$imageURLs[$i].jpg'/>";
-            unset($imageURLs[$randomIndex]);
             while($scores[$i] < 35){
                     $poppedValue = explode("_", array_pop($deck));
                     echo '<img src ="cards/'.$poppedValue[0].'/'.$poppedValue[1].'.png">';
-                    
-                    
                     $scores[$i] += $poppedValue[1];
                 }
-                echo "<p>". $score[$i] . "</p>";
+                echo $scores[$i] . "<br/>";
+                unset($imageURLs[$randomIndex]);
             }
            
-        
+        $_SESSION['matchcount'] += 1;
+          echo "Matches this session: " + $_SESSION['matchcount'] . "<br/>";
+          //Determines the highest value between players
+          $maxScore = $scores[0];
+          for($i = 0; $i < 4; $i++) {
+              if($scores[$i] > $maxScore) {
+                  $maxScore = $scores[$i];
+              }
+          }
+          //Determines winners for the round
+          $winner = array(0, 0, 0, 0);
+          for($i = 0; $i < 4; $i++) {
+              if($scores[$i] == $maxScore) {
+                  $winner[$i] = 1;
+              }
+          }
+          //Adds the scores together
+          $totalPoints = 0;
+          for($i = 0; $i < 4; $i++) {
+              if($winner[$i] == 0) {
+                  $totalPoints += $scores[$i];
+              }
+          }
+          //Assigns points to the winners
+          for($i = 0; $i < 4; $i++) {
+              if($winner[$i] == 1) {
+                  $_SESSION[''] += $totalPoints;
+              }
+          }
         ?>
         <form>
-            <input type="submit" value="Spin!"/>
+            <input type="submit" value="Replay!"/>
         </form>
         </div>
     </body>
